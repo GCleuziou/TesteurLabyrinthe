52fd46d6d8d4f6366ee7304827fd7f13b501bf21
GCleuziou <guillaume.cleuziou@gmail.com>
2020-02-24 20:50:55 +1100

    exo1 avec fonction couple

--- source/exercices/exo1.py
+++ source/exercices/exo1.py
@@ -1,15 +1,12 @@
 entrees_visibles = [
-        couple([1,2]),
-        couple([2,3])
+        (1,2),
+        (2,3)
 ]
 entrees_invisibles = [
-        couple([1,2]),
-        couple([2,3])
+        (1,2),
+        (2,3)
 ]
 
 @solution
-def couple(l):
-    return (l[0],l[1])
-
 def g(x,y):
   return y
